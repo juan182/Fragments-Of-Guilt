6fc6b6783bf5a2ed8d84fa5b52da26836468cc90
YenMur <yennifer.murillo@uao.edu.co>
2026-03-31 21:36:03 -0500

    EnemyPatrol

---
+++ ProyectoVideojuegos/Assets/Scripts/Enemies/EnemyPatrol.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+/// <summary>
+/// Gestiona el movimiento patrulla de los enemigos entre 2 puntos específicos
+/// </summary>
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform puntoA;
+    [SerializeField] private Transform puntoB;
+
+    [SerializeField] private float velocidad; // Velocidad a la que se mueve el enemigo
+
+    private Transform puntoObjetivo;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        if (puntoA == null || puntoB == null)
+        {
+            Debug.Log("No se han asignado los puntos de patrulla correctamente");
+        }
+
+        puntoObjetivo = puntoA;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distancia = Vector3.Distance(transform.position, puntoObjetivo.position);
+
+        if (distancia < 0.1f)
+        {
+            puntoObjetivo = (puntoObjetivo == puntoA) ? puntoB : puntoA;
+        }
+
+        Moverse();
+
+    }
+
+    /// <summary>
+    /// Desplaza al enemigo hacia un punto fisico especifico
+    /// </summary>
+
+    private void Moverse()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, puntoObjetivo.position, velocidad * Time.deltaTime);
+
+    }
+
+}
